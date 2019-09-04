111915b7e27b84be29e89b70a11fed940cbc416d
rajivmehtaflex <rajiv.mehta@live.in>
2019-09-04 12:28:26 +0530

    Start push to github

---
+++ app/server.py
@@ -0,0 +1,32 @@
+import aiohttp
+import asyncio
+import uvicorn
+import pathlib
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+path = pathlib.Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/ping', methods=['POST','GET'])
+async def analyze(request):
+    return JSONResponse(await request.json() )
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+    
