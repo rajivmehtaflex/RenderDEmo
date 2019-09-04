25dd9fc187e05ef649bbc9fca9aed72d05de32ee
Rajiv Mehta <rajiv.mehta@live.in>
2019-09-04 14:27:03 +0530

    Apply changes

--- app/server.py
+++ app/server.py
@@ -29,6 +29,6 @@ async def analyze(request):
 if __name__ == '__main__':
     if 'serve' in sys.argv:
         uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
-    else:
-        print("start Server....")
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+#     else:
+#         print("start Server....")
+#         uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
